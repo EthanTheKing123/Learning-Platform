a6822f6cf029cf231321a0149c36cd5e0428010d
EthanTheKing123 <ethankarpes@gmail.com>
2026-09-09 12:44:39 +1000

    Add files via upload

--- src/courses/index.js
+++ src/courses/index.js
@@ -1,5 +1,6 @@
 // Registers every course the platform offers.
-// To add a new course (e.g. Psychology): create src/courses/psychology.js
-// exporting a course object shaped like sleepScience.js, then add it here.
+// To add a new course: create src/courses/yourCourse.js exporting a course
+// object shaped like sleepScience.js, then add it here.
 import { sleepScience } from "./sleepScience.js";
-export const COURSES = [sleepScience];
+import { boostPerformance } from "./boostPerformance.js";
+export const COURSES = [sleepScience, boostPerformance];
