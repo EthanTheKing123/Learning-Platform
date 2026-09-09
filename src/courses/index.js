2d0e5c4a02f0eff1aaa261472ead360cab25e001
EthanTheKing123 <ethankarpes@gmail.com>
2026-09-09 15:18:40 +1000

    Update index.js

--- src/courses/index.js
+++ src/courses/index.js
@@ -4,4 +4,5 @@
 import { sleepScience } from "./sleepScience.js";
 import { boostPerformance } from "./boostPerformance.js";
 import { studyLearning } from "./studyskills.js";
-export const COURSES = [sleepScience, boostPerformance, studyLearning];
+import { chemistryoutcomes } from "./chemistryoutcomes.js";
+export const COURSES = [sleepScience, boostPerformance, studyLearning, chemistryoutcomes ];
