49efeabaaae20606ce2fa5fed1773a2e59b9b50d
EthanTheKing123 <ethankarpes@gmail.com>
2026-09-09 13:46:26 +1000

    Replace Studylearning with studyskills in COURSES

--- src/courses/index.js
+++ src/courses/index.js
@@ -3,5 +3,5 @@
 // object shaped like sleepScience.js, then add it here.
 import { sleepScience } from "./sleepScience.js";
 import { boostPerformance } from "./boostPerformance.js";
-import { Studylearning } from "./Studylearning.js";
-export const COURSES = [sleepScience, boostPerformance, Studylearning];
+import { studyskills } from "./studyskills.js";
+export const COURSES = [sleepScience, boostPerformance, studyskills];
